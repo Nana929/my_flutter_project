3a187a51bdf94e59833cc9ebb7757b760550edbf
lujieqian <qian0041@algonquinlive.com>
2025-02-04 17:41:57 -0500

    week4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+ 
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 122, 129, 112)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -25,6 +27,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+
   final String title;
 
   @override
@@ -32,338 +35,127 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //
+  var numCounter = 0;
+  var myFontSize = 30.0;
+  var isChecked = false;
+  String _loadedText = '';
+
+  final TextEditingController _num1 = TextEditingController();
+  final TextEditingController _num2 = TextEditingController();
+
+  void setNewValue(double value){ 
+    setState(() {
+      myFontSize = value;
+    });
+  }
+
+   @override
+  void initState() {
+    super.initState();
+    _loadSavedData();
+  }
+
+  void _loadSavedData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _num1.text = prefs.getString('saved_username') ?? ''; 
+      _num2.text = prefs.getString('saved_password') ?? ''; 
+    });
+  }
+
+
+  void _saveLoginData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('saved_username', _num1.text); // 存储用户名
+    await prefs.setString('saved_password', _num2.text); // 存储密码
+  }
+
+  Future<void> _removeData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('saved_username');
+    await prefs.remove('saved_password');
+    setState(() {
+      _num1.clear();
+      _num2.clear();
+    });
+  }
+
+  void _showAlertDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Notion'),
+          content: Text(
+              'Would you like to save your username and password for the next time ？'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                _removeData();
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Confirm'),
+              onPressed: () {
+                _saveLoginData();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-        body: Center(
-            // padding of the whole page
-            child: Padding(
-      padding: EdgeInsets.fromLTRB(12, 30, 12, 20),
+      appBar: AppBar(
 
-      // 1 column 8 rows
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment
-            .spaceBetween, // The Column( ) uses SpaceBetween for the layout
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        children: [
-          // 1st row: text to the center
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Column(
-              children: [
-                Text("browse categories".toUpperCase(),
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 2,
-                    )),
-              ],
-            ),
-          ]),
-
-          // 2nd row: text to the left
-          Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Expanded(
-              // expand text
-              child: Column(
-                children: [
-                  Text(
-                    "Not sure about exactly which recipe you're looking for? "
-                    "Do a search, or dive into our most popular categories.",
-                    style: TextStyle(fontSize: 15, letterSpacing: 1),
-                  ),
-                ],
-              ),
-            ),
-          ]),
-
-          // 3rd row: text to the center
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Column(
-              children: [
-                Text("by meat".toUpperCase(),
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 2)),
-              ],
-            ),
-          ]),
-
-          // 4th row: meat images
-          Row(
-              // 1 row, 4 columns
-              mainAxisAlignment: MainAxisAlignment
-                  .spaceAround, // The rows of images use SpaceAround to put spacing around them.
-              children: [
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/beef.jpg'),
-                          radius: 46),
-                      Text("beef".toUpperCase(),
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            shadows: [
-                              // add shadow to the text
-                              Shadow(
-                                offset: Offset(1.0, 1.0),
-                                blurRadius: 6.0,
-                                color: Colors.black,
-                              ),
-                            ],
-                          )),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/chicken.jpg'),
-                          radius: 46),
-                      Text(
-                        "chicken".toUpperCase(),
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          shadows: [
-                            // add shadow to the text
-                            Shadow(
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 6.0,
-                              color: Colors.black,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/pork.jpg'),
-                          radius: 46),
-                      Text("pork".toUpperCase(),
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            shadows: [
-                              // add shadow to the text
-                              Shadow(
-                                offset: Offset(1.0, 1.0),
-                                blurRadius: 6.0,
-                                color: Colors.black,
-                              ),
-                            ],
-                          )),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.center,
-                    children: [
-                      CircleAvatar(
-                          backgroundImage: AssetImage('images/seafood.jpg'),
-                          radius: 46),
-                      Text("seafood".toUpperCase(),
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            shadows: [
-                              // add shadow to the text
-                              Shadow(
-                                offset: Offset(1.0, 1.0),
-                                blurRadius: 6.0,
-                                color: Colors.black,
-                              ),
-                            ],
-                          )),
-                    ],
-                  ),
-                ]),
-              ]),
-
-          // 5th row: text to the center
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Column(
-              children: [
-                Text("by course".toUpperCase(),
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 2)),
-              ],
-            ),
-          ]),
-
-          // 6th row: course images
-          Row(
-              // 1 row, 4 columns
-              mainAxisAlignment: MainAxisAlignment
-                  .spaceAround, // The rows of images use SpaceAround to put spacing around them.
-              children: [
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                              backgroundImage:
-                                  AssetImage('images/main-dishes.jpg'),
-                              radius: 46),
-                          Text("Main Dishes", style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                              backgroundImage: AssetImage('images/salad.jpg'),
-                              radius: 46),
-                          Text("Salad", style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                              backgroundImage:
-                                  AssetImage('images/side-dishes.jpg'),
-                              radius: 46),
-                          Text("Side Dishes", style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                              backgroundImage:
-                                  AssetImage('images/crockpot.jpg'),
-                              radius: 46),
-                          Text("Crockpot", style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-              ]),
-
-          // 7th row: text to the center
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Column(
-              children: [
-                Text("by dessert".toUpperCase(),
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 2)),
-              ],
-            ),
-          ]),
-
-          // 8th row: dessert images
-          Row(
-              // 1 row, 4 columns
-              mainAxisAlignment: MainAxisAlignment
-                  .spaceAround, // The rows of images use SpaceAround to put spacing around them.
-              children: [
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                              backgroundImage:
-                                  AssetImage('images/ice—cream.jpg'),
-                              radius: 46),
-                          Text("Ice Cream", style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                              backgroundImage:
-                                  AssetImage('images/brownies.jpg'),
-                              radius: 46),
-                          Text("Brownies", style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar( 
-                              backgroundImage: AssetImage('images/pie.jpg'),
-                              radius: 46),
-                          Text("Pies", style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-                Column(children: [
-                  Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                              backgroundImage: AssetImage('images/cookies.jpg'),
-                              radius: 46),
-                          Text("Cookies", style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ]),
-              ]),
-        ],
+        title: Text(widget.title),
       ),
-    )));
-  }
-}
+      body: Center(
+
+        child: Column(
+        
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Image.asset("images/ac_logo.jpg", width: 300),
+            TextField(
+              controller: _num1,
+              decoration: const InputDecoration(
+                  hintText: "Please enter the login account:",
+                  labelText: "Login",
+                  border: OutlineInputBorder()),
+                   obscureText: true,
+            ),
+            TextField(
+              controller: _num2,
+              decoration: const InputDecoration(
+                  hintText: "Please enter the password:",
+                  labelText: "Password",
+                  border: OutlineInputBorder()),
+                   obscureText: true,
+            ),
+            
+            ElevatedButton(
+                onPressed: () {
+                  _showAlertDialog(context);
+
+                },
+                child: const Text("login"),
+                 ),
+          ],
+        ),
+      ),
+    );
+  }}
+
+                  
